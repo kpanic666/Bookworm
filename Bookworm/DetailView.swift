c21bdcb530eda886256dc638fa01c11143e36f42
Andrei Korikov <kpanic666@gmail.com>
2021-08-25 23:11:18 +0500

    Main features

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,83 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Andrei Korikov on 25.08.2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(book.genre ?? "Fantasy")
+                        .frame(maxWidth: geo.size.width)
+                    
+                    Text(book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+                
+                Text(book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text(book.review ?? "No review")
+                    .padding()
+                
+                RatingView(rating: .constant(Int(book.rating)))
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
+        }
+        .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+        .navigationBarItems(trailing: Button(action: {
+            showingDeleteAlert = true
+        }, label: {
+            Image(systemName: "trash")
+        }))
+        .alert(isPresented: $showingDeleteAlert, content: {
+            Alert(title: Text("Delete Book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete"), action: {
+                deleteBook()
+            }), secondaryButton: .cancel())
+        })
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        try? moc.save()
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+        
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
